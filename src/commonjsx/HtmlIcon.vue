13ae4b3cc2c3ba84fd90557a1b74a810e73024bc
chenxuanfeng <chenxuanfeng@meituan.com>
2019-02-01 00:34:44 +0800

    add

---
+++ src/commonjsx/HtmlIcon.vue
@@ -0,0 +1,32 @@
+<style scoped>
+.icon {
+  display: block;
+}
+</style>
+
+<script>
+export default {
+  name: "HtmlIcon",
+
+  props: {
+    src: {
+      type: String
+    }
+  },
+
+  methods: {},
+
+  render(h) {
+    return h(
+      "div",
+
+      {
+        class: "icon",
+        domProps: {
+          innerHTML: "<strong>喷出dom结构</strong>"
+        }
+      }
+    );
+  }
+};
+</script>
